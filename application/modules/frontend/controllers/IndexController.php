207933052575da5a9bf079196bceb8c3526ff2b1
valentin <fake.web.tv@gmail.com>
2013-05-03 17:57:48 +0300

    c

--- application/modules/frontend/controllers/IndexController.php
+++ application/modules/frontend/controllers/IndexController.php
@@ -4,7 +4,9 @@ class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        
+        $mapper = new Mapper_UserTable();
+        $result = $mapper->findCarsByModelId('en');
+        var_dump($result);
     }
 }
 
